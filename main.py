1a45da47d459a5b666ccb43c6376dfb7623ea8a8
networkdavit <lonecreation1612@gmail.com>
2021-12-03 10:01:27 +0400

    Making the app appear in the center

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from pytube import YouTube
 import threading
 
 root = tk.Tk()
-root.geometry("300x300")
+
+root_w = 300 # width for the Tk root
+root_h = 300 # height for the Tk root
+
+# get screen width and height
+root_ws = root.winfo_screenwidth() # width of the screen
+root_hs = root.winfo_screenheight() # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+root_x = (root_ws/2) - (root_w/2)
+root_y = (root_hs/2) - (root_h/2)
+
+
+root.geometry('%dx%d+%d+%d' % (root_w, root_h, root_x, root_y))
 root.title("Youtube Downloader")
 root.configure(bg="#14b6e2")
 root.iconbitmap("C:/Users/netwo/Desktop/youtube_downloader/youtube_icon.ico")
@@ -20,11 +33,23 @@ def download_youtube_video(link_to_video, name_for_video):
 	link_to_video = youtube_download_input.get()
 	name_for_video = video_name_input.get()
 	top= tk.Toplevel(root)
-	top.geometry("350x100")
+
+	top_w = 350 
+	top_h = 100 
+
+	top_ws = top.winfo_screenwidth() 
+	top_hs = top.winfo_screenheight() 
+
+	top_x = (top_ws/2) - (top_w/2)
+	top_y = (top_hs/2) - (top_h/2)
+
+	top.geometry('%dx%d+%d+%d' % (top_w, top_h, top_x, top_y))
 	top.title("Youtube Downloader")
-	tk.Label(top, text= "Downloading...", font=('Mistral 18 bold')).place(relx=.5, rely=.1,anchor= tk.CENTER)
+	top.configure(bg="#14b6e2")
+	top.iconbitmap("C:/Users/netwo/Desktop/youtube_downloader/youtube_icon.ico")
+	tk.Label(top, text= "Downloading...", font=('Mistral 18 bold')).place(relx=.5, rely=.2,anchor= tk.CENTER)
 	YouTube(link_to_video).streams.get_highest_resolution().download(filename=f"{name_for_video}.mp4", output_path="videos")
-	tk.Label(top, text= "Download complete!", font=('Mistral 18 bold')).place(relx=.5, rely=.1,anchor= tk.CENTER)
+	tk.Label(top, text= "Download complete!", font=('Mistral 18 bold')).place(relx=.5, rely=.2,anchor= tk.CENTER)
 	close_downloaded_popup = tk.Button(top, width = 16, height=3, text= "Back", bg="#e23e1e", command = lambda: top.destroy())
 	close_downloaded_popup.place(relx=.5, rely=.7,anchor= tk.CENTER)
 
